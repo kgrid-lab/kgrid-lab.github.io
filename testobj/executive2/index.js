5da5d3643fc6b90f9ea5f63c6205f0c73cde2dd8
George Meng <gqmeng.meng@gmail.com>
2020-04-22 16:16:34 -0400

    Update executive2

--- testobj/executive2/index.js
+++ testobj/executive2/index.js
@@ -1,31 +1,31 @@
-// const lcs = require('../../lcscreening/src/index')
-// const crcs = require('../../crcscreening/src/index')
-// const tests = require('../../testscreening/src/index')
-
 async function process(inputs){
   var results =[]
 
-  // Use the endpoint as key
-  // var crcs = global.cxt.getExecutor('knxW84nTAdyz')
-  // var lcs = global.cxt.getExecutor('knlcs')
-  // var tests =  global.cxt.getExecutor('kntests')
-
   // Use arkid, version, path
-  var crcs = global.cxt.getExecutorByID("ark:/99999/crcscreening", "v1.0.0", "qalygain")
-  var lcs = global.cxt.getExecutorByID("ark:/99999/lcscreening", "v1.0.1", "qalygain")
+  var cervcs = global.cxt.getExecutorByID("ark:/99999/cervcscreening", "1.0.0", "qalygain")
+  var crcs = global.cxt.getExecutorByID("ark:/99999/crcscreening", "1.0.0", "qalygain")
+  var lcs = global.cxt.getExecutorByID("ark:/99999/lcscreening", "1.0.1", "qalygain")
   //var tests =  global.cxt.getExecutor('kntests')
 
   var output ={}
-  output = await crcs.execute(inputs.patient)
-  results.push(output)
+  if(cervcs!=null) {
+    output = await cervcs.execute(inputs.patient)
+    results.push(output)
+  }else {
+    results.push({"id":"cervcscreening", "service": "Cervical Cancer Screening", "error":"KO not found."})
+  }
+  if(crcs!=null) {
+    output = await crcs.execute(inputs.patient)
+    results.push(output)
+  }else {
+    results.push({"id":"crcscreening", "service": "Colorectal Cancer Screening", "error":"KO not found."})
+  }
   if(lcs!=null) {
     output = await lcs.execute(inputs.patient)
     results.push(output)
   }else {
     results.push({"id":"lcscreening", "service": "Lung Cancer Screening", "error":"KO not found."})
   }
-  //output = await tests.execute(inputs.patient)
-  //results.push(output)
 
   /* Sort the result by the order of the QALY gain*/
   results.sort((a,b) => {
@@ -39,5 +39,4 @@ async function process(inputs){
   })
   return results;
 }
-
 module.exports = process
